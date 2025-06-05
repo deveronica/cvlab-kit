abe74025747877fc8a46e094890ddd24fd519550
정현서 <shu97@kau.kr>
2025-06-05 18:28:44 +0900

    Revert "wandb-voc"
    This reverts commit 74c2b866fb9c11931e26a3d02797bbaa15af2a71.

--- cvlabkit/agent/basic.py
+++ cvlabkit/agent/basic.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import DataLoader
 from cvlabkit.core.agent import Agent as BaseAgent
 from cvlabkit.core.creator import Creator
-import wandb
+
 
 def collate_fn(batch):
     return [b["img"] for b in batch], [b["target"] for b in batch]
@@ -16,15 +16,6 @@ class Agent(BaseAgent):
         self.model = create.model()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
-
-        # wandb start (오른쪽 인자값은 기본값. config 작성된 이름을 우선적으로 사용)
-        self.wandb_run = wandb.init(
-            project = self.cfg.get("wandb_project", "sfda-detection"),
-            name = self.cfg.get("wandb_run_name", "basic-agent-run-voc"),
-            config = self.cfg.to_dict()
-        )
-        wandb.define_metric("epoch")
-        wandb.define_metric("metrics/*", step_metric="epoch")
 
     def dry_run(self):
         loader = DataLoader(self.dataset, batch_size=1, collate_fn=collate_fn)
@@ -62,11 +53,6 @@ class Agent(BaseAgent):
 
                 total_loss += loss.item()
                 num_batches += 1
-                
-                ## wandb setp log add
-                wandb.log({
-                    "train/loss_step": loss.item(),
-                })
 
             avg_loss = total_loss / num_batches
             print(f"[fit] Epoch {epoch + 1}: Avg Loss = {avg_loss:.4f}")
@@ -74,18 +60,6 @@ class Agent(BaseAgent):
             results = self.evaluate()
             print(f"[fit] Epoch {epoch + 1}: AP@[.50:.95] = {results['AP@[.50:.95]']:.4f}, AP50 = {results['AP50']:.4f}, AP75 = {results['AP75']:.4f}")
             
-                        # wandb epoch log add
-            wandb.log({
-                "epoch" : epoch,
-                "metrics/avg_loss" : avg_loss,
-                "metrics/AP@[.50:.95]": results["AP@[.50:.95]"],
-                "metrics/AP50": results["AP50"],
-                "metrics/AP75": results["AP75"]
-            })
-        
-        # wandb finish
-        wandb.finish()
-
     def evaluate(self):
         self.model.eval()
         loader = DataLoader(self.dataset, batch_size=1, collate_fn=collate_fn)
